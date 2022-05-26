b9e9da876ba82cfa48ad47e4cd208717c9249291
bhautik mehtam <bmehta217@gmail.com>
2022-05-26 20:55:44 +0530

    changes camera module

--- src/app/auth/camera/camera.component.ts
+++ src/app/auth/camera/camera.component.ts
@@ -1,11 +1,15 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { AuthService } from '../core/_services/auth.services';
 @Component({
   selector: 'shared-camera',
   templateUrl: './camera.component.html',
   styleUrls: ['./camera.component.scss'],
 })
 export class CameraComponent implements OnInit {
+
+  @Input() position = 'floating';
+  @Input() mode = "mode";
+  @Output() cameraApiOutput = new EventEmitter();
 
   constructor() { }
 
