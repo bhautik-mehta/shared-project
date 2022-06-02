4d1d934c30cc7362a6d425bf91f19ae11683fa44
bhautik mehtam <bmehta217@gmail.com>
2022-06-02 11:53:39 +0530

    changes

--- src/app/auth/camerahome/camerahome.component.ts
+++ src/app/auth/camerahome/camerahome.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-camerahome',
+  selector: 'shared-camerahome',
   templateUrl: './camerahome.component.html',
   styleUrls: ['./camerahome.component.scss'],
 })
