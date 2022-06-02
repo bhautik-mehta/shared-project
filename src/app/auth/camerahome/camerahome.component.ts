b6071784b543f85d84f2d97922ca0c0ad68f5778
bhautik mehtam <bmehta217@gmail.com>
2022-06-02 14:55:19 +0530

    changes

--- src/app/auth/camerahome/camerahome.component.ts
+++ src/app/auth/camerahome/camerahome.component.ts
@@ -1,6 +1,9 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Router } from '@angular/router';
-
+import { AngularFireStorage } from "@angular/fire/compat/storage";
+import { AuthService } from '../core/_services/auth.services';
+import { map, finalize } from "rxjs/operators";
+import { Observable } from "rxjs";
 @Component({
   selector: 'shared-camerahome',
   templateUrl: './camerahome.component.html',
@@ -11,8 +14,37 @@ export class CamerahomeComponent implements OnInit {
   @Input() mode = "mode";
   @Output() cameraHomeApiOutput = new EventEmitter();
 
-  constructor() { }
-
+  title = "cloudsSorage";
+  selectedFile: File = null;
+  fb;
+  downloadURL: Observable<string>;
+  constructor(private Api: AuthService, private storage: AngularFireStorage) { }
   ngOnInit() { }
+  onFileSelected(event) {
+    var n = Date.now();
+    const file = event.target.files[0];
+    const filePath = `RoomsImages/${n}`;
+    const fileRef = this.storage.ref(filePath);
+    const task = this.storage.upload(`RoomsImages/${n}`, file);
+    task
+      .snapshotChanges()
+      .pipe(
+        finalize(() => {
+          this.downloadURL = fileRef.getDownloadURL();
+          this.downloadURL.subscribe(url => {
+            if (url) {
+              this.fb = url;
+            }
+            console.log(this.fb);
+          });
+        })
+      )
+      .subscribe(url => {
+        if (url) {
+          console.log(url);
+        }
+      });
+  }
+
 
 }
